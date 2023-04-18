20ea3122b75d54af9f361adc6226dac34f4a21cd
Slava <slava@qey.ai>
2023-04-18 14:39:44 +0300

    Download with wget

--- autoconf/src/main/jni/build.sh
+++ autoconf/src/main/jni/build.sh
@@ -1,2 +1,3 @@
-git clone --depth=1 --branch=v2.72c git://git.savannah.gnu.org/autoconf.git autoconf
-rm -rf ./autoconf/.git
+wget https://github.com/autotools-mirror/autoconf/archive/refs/tags/v2.72c.tar.gz
+#git clone --depth=1 --branch=v2.72c git://git.savannah.gnu.org/autoconf.git autoconf
+#rm -rf ./autoconf/.git
